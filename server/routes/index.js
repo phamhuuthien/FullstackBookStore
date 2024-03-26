00daa61661d80fe43acc89cfb1884d0e0bd027d8
Nguyễn Phúc Hiệp <132082516+DraiGShyva@users.noreply.github.com>
2024-03-26 23:18:50 +0700

    Add coupon and order

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,13 @@
 
 const roleRouters = require('./roleRouter')
+const orderRouters = require('./orderRouter')
+const couponRouters = require('./couponRouter')
 
 
-const initRouters = (app)=>{
-    app.use("/api/v1/role",roleRouters)
+const initRouters = (app) => {
+    app.use("/api/v1/role", roleRouters)
+    app.use("/api/v1/order", orderRouters)
+    app.use("/api/v1/coupon", couponRouters)
 }
 
 module.exports = initRouters;
