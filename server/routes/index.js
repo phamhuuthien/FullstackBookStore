7bd021d7db034aa404b6803606183372e05c4884
nguyendazero <nguyendazero@gmail.com>
2024-05-02 10:09:14 +0700

    add comments function

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,6 +5,7 @@ const userRouters = require("./userRouter");
 const orderRouters = require("./orderRouter");
 const couponRouters = require("./couponRouter");
 const bookRouter = require("./bookRouter");
+const commentRouter = require("./commentRouter");
 
 const initRouters = (app) => {
   app.use("/api/v1/role", roleRouters);
@@ -12,6 +13,7 @@ const initRouters = (app) => {
   app.use("/api/v1/order", orderRouters);
   app.use("/api/v1/coupon", couponRouters);
   app.use("/api/v1/books", bookRouter);
+  app.use("/api/v1/comments", commentRouter);
 };
 
 module.exports = initRouters;
