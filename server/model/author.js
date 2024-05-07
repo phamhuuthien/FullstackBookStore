5640d8f44da1a78e52212719c34948c5329ad27e
ly <letucongly@gmail.com>
2024-05-07 21:24:34 +0700

    upload author category

--- server/model/author.js
+++ server/model/author.js
@@ -1,1 +1,29 @@
-console.log("author 1")
+const mongoose = require('mongoose');
+
+const authorSchema = new mongoose.Schema({
+    name: {
+        type: String,
+    },
+    biography: {
+        type: String,
+    },
+    image: {
+        type: String,
+    },
+    nationality: {
+        type: String,
+    },
+    birthDate: {
+        type: Date,
+    },
+    books: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Book'
+    }],
+    slug: {
+        type: String,
+    }
+});
+
+
+module.exports = mongoose.model("Author", authorSchema);
