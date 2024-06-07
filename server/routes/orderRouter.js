e273676d9a005a495790678bdcefe096b6a9cbc8
phamhuuthien <phamhuuthien2608@gmail.com>
2024-06-07 11:25:13 +0700

    refactor logic order

--- server/routes/orderRouter.js
+++ server/routes/orderRouter.js
@@ -2,8 +2,11 @@ const router = require('express').Router();
 const controller = require('../controller/orderController')
 const { verifyToken, isAdmin } = require('../middlewares/verifyToken')
 
-router.get("/:uid", verifyToken, controller.getOrder);
-router.post("/:uid", verifyToken, controller.addOrder);
+router.get("/getAllOrderByUser", verifyToken, controller.getAllOrderByUser);
+
+router.get("/:oid", verifyToken, controller.getOrder);
+
+router.post("/", verifyToken, controller.addOrder);
 
 router.get("/", verifyToken, isAdmin, controller.getAllOrder);
 router.put("/:oid", verifyToken, isAdmin, controller.updateOrder);
